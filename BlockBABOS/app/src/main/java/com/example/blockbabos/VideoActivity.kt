9365b08e0b2a28a2f7728a28e7938848b1570861
Benny <villiger@protonmail.com>
2020-10-04 22:23:35 +0200

    just a test with swipe gesture and youtubeplayer

--- BlockBABOS/app/src/main/java/com/example/blockbabos/VideoActivity.kt
+++ BlockBABOS/app/src/main/java/com/example/blockbabos/VideoActivity.kt
@@ -12,10 +12,21 @@ import com.google.android.youtube.player.YouTubePlayerView
 
 class VideoActivity : YouTubeBaseActivity(){
 
+    var hardcodedVideoList = ArrayList<String>()
+    lateinit var youtubePlayer :YouTubePlayer
+    var i = 0
+
+
     //TODO fix reloading of video, everytime the phone gets turned
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+
+        hardcodedVideoList.add("cN9G4v2jZnM")
+        hardcodedVideoList.add("CR7ob354hrA")
+        hardcodedVideoList.add("hWfaUdNPCYM")
+        hardcodedVideoList.add("4CgCbigmk7o")
+        hardcodedVideoList.add("nOK84lLkvM0")
 
         val youTubePlayerView = findViewById<View>(R.id.youtube_player) as YouTubePlayerView
 
@@ -25,6 +36,7 @@ class VideoActivity : YouTubeBaseActivity(){
                     provider: YouTubePlayer.Provider,
                     youTubePlayer: YouTubePlayer, b: Boolean
                 ) {
+                    youtubePlayer = youTubePlayer
                     youTubePlayer.cueVideo("N1WRualRBOQ")
                     youTubePlayer.setShowFullscreenButton(false)
                     youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.MINIMAL)
@@ -42,8 +54,16 @@ class VideoActivity : YouTubeBaseActivity(){
     fun onSwipe (type: Swipe.SwipeType){
         val logTag = "WHATEVER"
 
+
+        //Just a test
+        youtubePlayer.cueVideo(hardcodedVideoList.get(i++))
+        youtubePlayer.play()
+
         fun onRightToLeftSwipe() {
             Log.i(logTag, "RightToLeftSwipe!")
+
+
+
             Toast.makeText(this, "RightToLeftSwipe", Toast.LENGTH_SHORT).show()
             // activity.doSomething();
         }
