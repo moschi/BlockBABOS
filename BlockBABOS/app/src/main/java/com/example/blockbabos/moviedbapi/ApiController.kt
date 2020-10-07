87a8e4fc6aa60121998ea860c4859fb920e96ef4
Benny <villiger@protonmail.com>
2020-10-07 19:49:33 +0200

    now we use our own httpclient, since the one from the moviedb is incompatible with android

--- BlockBABOS/app/src/main/java/com/example/blockbabos/moviedbapi/ApiController.kt
+++ BlockBABOS/app/src/main/java/com/example/blockbabos/moviedbapi/ApiController.kt
@@ -1,30 +1,83 @@
 package com.example.blockbabos.moviedbapi
 
-import com.omertron.themoviedbapi.TheMovieDbApi
+import android.os.AsyncTask
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.omertron.themoviedbapi.model.AbstractJsonMapping
+import com.omertron.themoviedbapi.model.media.Video
+import com.omertron.themoviedbapi.model.movie.MovieBasic
 import com.omertron.themoviedbapi.model.movie.MovieInfo
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.hc.client5.http.classic.methods.ClassicHttpRequests
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
+import org.apache.hc.core5.http.ClassicHttpRequest
+import java.io.BufferedInputStream
+
 
 class ApiController {
 
     private val API_KEY = "f6789f8a7c4ea3cc8c3b4fc4b1e75973"
     private val LANGUAGE = "en_US"
+    private val BASE_URI = "https://api.themoviedb.org/3/"
     private val httpClient: CloseableHttpClient = HttpClientBuilder.create().build()
 
-    private val theMovieDbAApi = TheMovieDbApi(API_KEY, httpClient)
+    private val TOP_RATED = "movie/top_rated"
+    private val POPULAR = "movie/popular"
+    val mapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(
+        JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
 
-    fun getTopRatedMovies(): List<MovieInfo> {
-        val topRatedMovies = theMovieDbAApi.getTopRatedMovies(1, LANGUAGE)
-        return topRatedMovies.results
+
+    fun createRequest (uri:String): ClassicHttpRequest{
+        val request = ClassicHttpRequests.get(uri)
+        return request
+    }
+
+    fun uriBuilder (path: String, isList: Boolean): String{
+        if(isList){
+            return "$BASE_URI$path?api_key=$API_KEY&language=$LANGUAGE&page=1"
+
+        }else{
+            return "$BASE_URI$path?api_key=$API_KEY"
+        }
+    }
+
+//    inline fun <reified T:AbstractJsonMapping> castResponse(response: CloseableHttpResponse) : T {
+//        val inputStream = BufferedInputStream(response.entity.content)
+//        val responseString = IOUtils.toString(inputStream)
+//        println(responseString)
+//        return mapper.readValue(responseString)
+//    }
+    inline fun <reified T:List<AbstractJsonMapping>> castResponseList(response: CloseableHttpResponse) : T {
+        val inputStream = BufferedInputStream(response.entity.content)
+        val jsonObject = mapper.readTree(inputStream)
+        response.close()
+        val resultObject = jsonObject.findValue("results")
+        return mapper.readValue(resultObject.toString())
+    }
+
+    fun getTopRatedMovies(): List<MovieBasic> {
+        val uri = (uriBuilder(TOP_RATED, true))
+        println(uri)
+        val response = httpClient.execute(createRequest(uri))
+        return castResponseList(response)
     }
 
     fun getMostViewedMovies(): List<MovieInfo> {
-        val mostViewed = theMovieDbAApi.getPopularMovieList(1, LANGUAGE)
-        return mostViewed.results
+        val uri = (uriBuilder(POPULAR, true))
+        println(uri)
+        val response = httpClient.execute(createRequest(uri))
+        return castResponseList(response)
     }
 
-    fun getTrailerLink(movieInfo: MovieInfo): String {
-        val movieVideos = theMovieDbAApi.getMovieVideos(movieInfo.id, LANGUAGE)
-        return movieVideos.results[0].key
+    fun getTrailerLinks(movieInfo: MovieInfo): List<Video> {
+        val uri = (uriBuilder("movie/" + movieInfo.id.toString() + "/videos", true))
+        println(uri)
+        val response = httpClient.execute(createRequest(uri))
+        return castResponseList(response)
     }
+
 }
--- BlockBABOS/app/src/test/java/com/example/blockbabos/moviedbapi/ApiControllerTest.kt
+++ BlockBABOS/app/src/test/java/com/example/blockbabos/moviedbapi/ApiControllerTest.kt
@@ -3,24 +3,30 @@ package com.example.blockbabos.moviedbapi
 import org.junit.Assert
 import org.junit.Test
 
-class ApiControllerTest{
+class ApiControllerTest {
 
-    private val restConverter = ApiController()
+    private val apiController = ApiController()
 
     @Test
-    fun test(){
-        val topRatedMovies = restConverter.getTopRatedMovies()
+    fun topRated() {
+        val topRatedMovies = apiController.getTopRatedMovies()
         println("top rated movie: " + topRatedMovies[0].title)
         Assert.assertNotNull(topRatedMovies)
     }
 
     @Test
-    fun trailerLink(){
-        val link = restConverter.getTrailerLink(restConverter.getMostViewedMovies()[0])
-        println("link of top rated movie is: $link")
-        Assert.assertTrue(link != "")
+    fun trailerLink() {
+        val links = apiController.getTrailerLinks(apiController.getMostViewedMovies()[0])
+        println("links of top rated movie is: $links")
+        Assert.assertTrue(links[0].toString() != "")
     }
 
+    @Test
+    fun mostViewed() {
+        val mostViewed = apiController.getMostViewedMovies()[0]
+        println("Most viewed movie is: $mostViewed")
+        Assert.assertNotNull(mostViewed)
+    }
 
 
 }
