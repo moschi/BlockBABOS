f77521c7d18c6da6f04149c584e8eeda026b8527
David Kühnhanss <david.kuehnhanss@gmail.com>
2020-10-27 15:28:03 +0100

    clean up

--- BlockBABOS/app/src/main/java/com/example/blockbabos/presentation/fragments/ListFragment.kt
+++ BlockBABOS/app/src/main/java/com/example/blockbabos/presentation/fragments/ListFragment.kt
@@ -20,11 +20,6 @@ import com.example.blockbabos.presentation.ListViewModel
 import com.example.blockbabos.presentation.ListViewModelFactory
 import com.google.android.material.appbar.MaterialToolbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [ListFragment.newInstance] factory method to
