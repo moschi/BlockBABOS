dc901f1d0be891c9ecc0da96005d33fb4da839ee
Benny <villiger@protonmail.com>
2020-10-27 16:38:43 +0100

    refactoring and bugfix

--- BlockBABOS/app/src/main/java/com/example/blockbabos/domain/moviedbapi/ApiController.kt
+++ BlockBABOS/app/src/main/java/com/example/blockbabos/domain/moviedbapi/ApiController.kt
@@ -15,7 +15,6 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder
 import org.apache.hc.core5.http.ClassicHttpRequest
 import java.io.BufferedInputStream
-import java.lang.RuntimeException
 
 private const val API_KEY = "f6789f8a7c4ea3cc8c3b4fc4b1e75973"
 private const val LANGUAGE = "en_US"
@@ -25,33 +24,35 @@ private const val POPULAR = "movie/popular"
 
 class ApiController {
     private val httpClient: CloseableHttpClient = HttpClientBuilder.create().build()
-    private val mapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).configure(
-        JsonParser.Feature.AUTO_CLOSE_SOURCE, true)
+    private val mapper: ObjectMapper =
+        jacksonObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(
+                JsonParser.Feature.AUTO_CLOSE_SOURCE, true
+            )
 
-    private fun createRequest (uri:String): ClassicHttpRequest{
+    private fun createRequest(uri: String): ClassicHttpRequest {
         return ClassicHttpRequests.get(uri)
     }
 
-    private fun uriBuilder (path: String, isList: Boolean): String{
-        return if(isList){
+    private fun uriBuilder(path: String, isList: Boolean): String {
+        return if (isList) {
             "$BASE_URI$path?api_key=$API_KEY&language=$LANGUAGE&page=1"
 
-        }else{
+        } else {
             "$BASE_URI$path?api_key=$API_KEY"
         }
     }
 
-    private inline fun <reified T:List<AbstractJsonMapping>> castResponseList(response: CloseableHttpResponse) : T {
+    private inline fun <reified T : List<AbstractJsonMapping>> castResponseList(response: CloseableHttpResponse): T {
         val inputStream = BufferedInputStream(response.entity.content)
         val jsonObject = mapper.readTree(inputStream)
         response.close()
         val resultObject = jsonObject.findValue("results")
-        //todo here's a bug!
-        if(resultObject != null) {
-            return mapper.readValue(resultObject.toString())
-        }else{
-            throw RuntimeException("Could not parse result object")
+        var returnValue = "[]"
+        if (resultObject != null) {
+            returnValue = resultObject.toString()
         }
+        return mapper.readValue(returnValue)
     }
 
     fun getTopRatedMovies(): List<MovieBasic> {
@@ -75,7 +76,7 @@ class ApiController {
     }
 
     fun getSimilar(movieId: Int): List<MovieInfo> {
-        var uri = (uriBuilder("movie/$movieId/similar", true))
+        val uri = (uriBuilder("movie/$movieId/similar", true))
         val response = httpClient.execute(createRequest(uri))
         return castResponseList(response)
     }
